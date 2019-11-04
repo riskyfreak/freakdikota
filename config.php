6e437dbae6b50d26e29ecbaa86c8a99f9f91599f
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-11-04 20:29:41 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 	
 $link = "http://ugg.idkota.com/Invite/invite_friends/MTkzMDk5?lang=indonesian&package=com.earnmoney.idinfo";
 
-$katasandi = "dikotafreak666";
+$katasandi = "pejuangreceh";
 
 $member_id = "193099";
 

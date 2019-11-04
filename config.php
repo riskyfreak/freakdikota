1999d60e306b940627507ff55347551ed24033a9
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-11-04 20:22:27 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Coli Dulu Bosssss 😁😁😁😁😁
+	
+$link = "http://ugg.idkota.com/Invite/invite_friends/MTkzMDk5?lang=indonesian&package=com.earnmoney.idinfo";
+
+$katasandi = "dikotafreak666";
+
+$member_id = "193099";
+

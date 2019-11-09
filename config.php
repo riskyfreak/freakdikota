b2bfc5748bb298c7b06a5904df1bfadd21d36b65
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-11-08 19:02:51 -0800

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 // Coli Dulu Bosssss 😁😁😁😁😁
 	
-$link = "http://ugg.idkota.com/Invite/invite_friends/MTkzMDk5?lang=indonesian&package=com.earnmoney.idinfo";
+$link = "http://ugg.idkota.com/Invite/invite_friends/MjIxMjAw?lang=indonesian&package=com.earnmoney.idinfo";
 
 $katasandi = "pejuangreceh";
 
-$member_id = "193099";
+$member_id = "221200";
 
